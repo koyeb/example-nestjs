a5c53ff7583faaa49d73b4fb796cfe310adc546a
m9236059933 <m9236059933@gmail.com>
2024-10-27 19:29:36 -0700

    remove old data

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World Dev 2!';
-  }
-}
+export class AppService {}
